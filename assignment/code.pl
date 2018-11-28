dac47e331239df946e54fd5c80d25431908871f4
Rohit Rajhans <rohitrajhans07@gmail.com>
2018-11-28 19:48:00 +0530

    adapter clause added.

--- assignment/code.pl
+++ assignment/code.pl
@@ -1,14 +1,31 @@
-packet() :-
+not_empty(L) :-
+    length(L, K),
+    K>0.
+
+isAdapterValid(X) :-
+    char_code(X, Code),
+    Code =< 72,
+    Code >= 65.
+
+allow() :-
+    write('Packet is rejected'),
     false.
 
-packet(X) :-
-    adapter(X).
+allow(L) :-
+    adapter_validation(L),
+    format('Packet with adapter ~w is allowed', [L]).
 
-adapter(X) :-
-    char_code(X,Y),
-    Y < 73.
+adapter_validation([]) :-
+    true.
 
-adapter(X) :-
-    char_code(X,Y),
-    Y > 72,
-    format('Packet with adapter "any" is rejected', [X]).
+adapter_validation([H|T]) :-
+    not_empty([H|T]),
+    isAdapterValid(H),
+    adapter_validation(T).
+    
+
+/* checkIfCharValid(X) :-
+    char_code(X, Code),
+    Code >= 65,
+    Code =< 91.
+*/

b0fe2e5bab583ff2d3c45d7baa593c7de77903a5
Rohit Rajhans <rohitrajhans07@gmail.com>
2018-11-28 21:01:34 +0530

    added ethernet and solved jugaad

--- assignment/code.pl
+++ assignment/code.pl
@@ -3,13 +3,12 @@ not_empty(L) :-
     K>0.
 
 allow() :-
-    write('Packet is rejected'),
     false.
 
 allow(L, M) :-
     adapter_validation(L),
     ethernet_validation(M),
-    format('Packet with adapter ~w is allowed', [L]).
+    format('Packet is allowed').
 
 /* Adapter Clauses */
 
@@ -18,13 +17,16 @@ isAdapterValid(X) :-
     Code =< 72,
     Code >= 65.
 
-adapter_validation([]) :-
+adapter_validation(L) :-
+    not_empty(L),
+    adapter_validation_rec(L).
+
+adapter_validation_rec([]) :-
     true.
 
-adapter_validation([H|T]) :-
-    not_empty([H|T]),
+adapter_validation_rec([H|T]) :-
     isAdapterValid(H),
-    adapter_validation(T).
+    adapter_validation_rec(T).
     
 
 /* checkIfCharValid(X) :-
@@ -34,3 +36,40 @@ adapter_validation([H|T]) :-
 */
 
 /* Ethernet Clauses */
+
+/* ethernet clause input [ [vlan-numbers], [proto-ids in hex form]] */
+
+ethernet_validation([V|[P|_]]) :-
+    vlan_validation(V),
+    proto_validation(P).
+
+vlan_validation(L) :-
+    not_empty(L),
+    vlan_validation_rec(L).
+
+vlan_validation_rec([]) :-
+    true.
+
+vlan_validation_rec([H|T]) :-
+    not_empty([H|T]),
+    isVlanNumberValid(H),
+    vlan_validation_rec(T).
+
+isVlanNumberValid(H) :-
+     H >= 1,
+     H =< 999.
+
+proto_validation(L) :-
+    not_empty(L),
+    proto_validation_rec(L).
+
+proto_validation_rec([]) :-
+    true.
+
+proto_validation_rec([H|T]) :-
+    isProtoIdValid(H),
+    proto_validation_rec(T).
+
+isProtoIdValid(H) :-
+    H = '0x0800';
+    H = '0x86dd'.

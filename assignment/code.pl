a1f04bc119ee8bf55c08d96a5c0ddffb690a3871
Rohit Rajhans <rohitrajhans07@gmail.com>
2018-11-29 17:26:14 +0530

    code rewritten

--- assignment/code.pl
+++ assignment/code.pl
@@ -1,75 +1,25 @@
-not_empty(L) :-
-    length(L, K),
-    K>0.
+consult(database).
 
-allow() :-
-    false.
 
-allow(L, M) :-
-    adapter_validation(L),
-    ethernet_validation(M),
-    format('Packet is allowed').
+packet(X, Y) :-
+    allow(X, Y),
+    write('Packet allowed').
 
-/* Adapter Clauses */
+packet(X, Y) :-
+    reject(X, Y),
+    write('Packet rejected').
 
-isAdapterValid(X) :-
-    char_code(X, Code),
-    Code =< 72,
-    Code >= 65.
+packet(X, Y) :-
+    drop(X, Y).
 
-adapter_validation(L) :-
-    not_empty(L),
-    adapter_validation_rec(L).
+allow(X, Y) :-
+    adapter(X),
+    ethernet(Y).
 
-adapter_validation_rec([]) :-
-    true.
+drop(X, Y) :-
+    \+adapter(X),
+    \+reject_adapter(X).
 
-adapter_validation_rec([H|T]) :-
-    isAdapterValid(H),
-    adapter_validation_rec(T).
-    
-
-/* checkIfCharValid(X) :-
-    char_code(X, Code),
-    Code >= 65,
-    Code =< 91.
-*/
-
-/* Ethernet Clauses */
-
-/* ethernet clause input [ [vlan-numbers], [proto-ids in hex form]] */
-
-ethernet_validation([V|[P|_]]) :-
-    vlan_validation(V),
-    proto_validation(P).
-
-vlan_validation(L) :-
-    not_empty(L),
-    vlan_validation_rec(L).
-
-vlan_validation_rec([]) :-
-    true.
-
-vlan_validation_rec([H|T]) :-
-    not_empty([H|T]),
-    isVlanNumberValid(H),
-    vlan_validation_rec(T).
-
-isVlanNumberValid(H) :-
-     H >= 1,
-     H =< 999.
-
-proto_validation(L) :-
-    not_empty(L),
-    proto_validation_rec(L).
-
-proto_validation_rec([]) :-
-    true.
-
-proto_validation_rec([H|T]) :-
-    isProtoIdValid(H),
-    proto_validation_rec(T).
-
-isProtoIdValid(H) :-
-    H = '0x0800';
-    H = '0x86dd'.
+reject(X, Y) :-
+    \+adapter(X),
+    reject_adapter(X).

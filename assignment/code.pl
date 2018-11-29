9e6771779fb035ef50a2be643c93c5e620ab86ad
Rohit Rajhans <rohitrajhans07@gmail.com>
2018-11-29 23:35:04 +0530

    bug fixes

--- assignment/code.pl
+++ assignment/code.pl
@@ -1,70 +1,69 @@
 packet(X, Y) :-
-    validate_adapter(X),
-    validate_ethernet(Y).
+    is_packet_accepted(X, Y),
+    write('Packet accepted').
 
-validate_adapter(X) :-
-    allow(L, _),
+is_packet_accepted(X,Y) :-
+    allow(L, M),
+    validate_adapter(X,L),
+    validate_ethernet(Y,M).
+
+validate_adapter('', L) :-
+    validate_adapter('Z', L).
+
+validate_adapter(X, L) :-
     split_string(L, "-", "", T),
     length(T, I),
     I=2,
     memberOfRange(X,T).
     
-validate_adapter(X) :-
-    allow(L, _),
+validate_adapter(X, L) :-
     split_string(L, ",", "", T),
     length(T, I),
     I >= 2,
     memberOfList(X, T).
 
-validate_adapter(X) :-
-    allow(P, _),
-    string_length(P, I),
+validate_adapter(X, L) :-
+    string_length(L, I),
     I=1,
     char_code(X, XC),
-    char_code(P, PC),
+    char_code(L, PC),
     XC=PC.
 
-validate_ethernet([V|[P|_]]) :-
-    validate_vid(V),
-    validate_proto(P).
+validate_ethernet([V|[P|_]], L) :-
+    validate_vid(V, L),
+    validate_proto(P, L).
 
-validate_vid(X) :-
-    allow(_, [E|_]),
+validate_vid(X, [E|_]) :-
     split_string(E, "-", "", T),
     length(T, I),
     I=2,
     memberOfNumberRange(X,T).
 
-validate_vid(X) :-
-    allow(_, [E|_]),
+validate_vid(X, [E|_]) :-
     split_string(E, ",", "", T),
     length(T, I),
     I >= 2,
     memberOfNumberList(X, T).
 
-validate_vid(X) :-
-    allow(_, [E|_]),
+validate_vid(X, [E|_]) :-
     string_to_list(E, S),
     checkValidNumber(S),
     atom_number(E, EN),
     X=EN.
 
-validate_proto(X) :-
-    allow(_, [_|[E|_]]),
+validate_proto(X, [_|[E|_]]) :-
     split_string(E, "-", "", T),
     length(T, I),
     I=2,
     memberOfNumberRange(X,T).
 
-validate_proto(X) :-
-    allow(_, [_|[E|_]]),
+validate_proto(X, [_|[E|_]]) :-
     split_string(E, ",", "", T),
     length(T, I),
     I >= 2,
     memberOfNumberList(X, T).
 
-validate_proto(X) :-
-    allow(_, [_|[E|_]]),
+validate_proto(X, [_|[E|_]]) :-
     string_to_list(E, S),
     checkValidNumber(S),
     atom_number(E, EN),
